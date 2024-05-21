12d49b192781870195d12c888cb79266ff3f2b01
Lorenzo Aceti <lorenzo.aceti@isdbflowtech.com>
2024-05-21 17:21:58 -0300

    feat: add theme switcher (unfinished)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Sidebar } from '@/components/core/sidebar'
+import ThemeProvider from '@/components/providers/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
 import { fira } from '@/lib/fonts'
 import type { Metadata } from 'next'
 import './globals.css'
@@ -18,11 +20,29 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`font-fira ${fira.variable} bg-slate-900 text-slate-50 antialiased`}
+        suppressHydrationWarning
       >
-        <div className="grid h-screen grid-cols-[18rem_1fr]">
-          <Sidebar />
-          <main className="flex flex-1 flex-col">{children}</main>
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="grid h-screen grid-cols-[18rem_1fr]">
+            <Sidebar />
+            <main className="flex flex-1 flex-col">{children}</main>
+          </div>
+          <Toaster
+            richColors
+            expand={true}
+            visibleToasts={1}
+            toastOptions={{
+              style: {
+                backgroundColor: '#1e293b',
+              },
+            }}
+          />
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/components/core/sidebar.tsx
+++ src/components/core/sidebar.tsx
@@ -2,15 +2,18 @@
 
 import Logo from '@/assets/logo.png'
 import { navConfig } from '@/config/nav'
+import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { Switch } from '../ui/switch'
 
 export function Sidebar() {
   const pathname = usePathname()
+  const { resolvedTheme, setTheme } = useTheme()
 
   return (
-    <aside className="border-border flex flex-col space-y-6 border-r">
+    <aside className="flex flex-col space-y-6 border-r border-slate-800">
       <header className="flex items-center gap-3 border-b border-slate-800 p-6">
         <Image src={Logo} alt="Pocket Trader's logo." width={24} height={24} />
         <h1 className="text-xl font-semibold text-slate-50">
@@ -19,22 +22,34 @@ export function Sidebar() {
       </header>
 
       <main className="grow flex-col px-3">
-        <nav>
-          <main className="flex flex-col gap-2">
-            {navConfig.map(({ title, href, icon: Icon }) => (
-              <Link
-                key={title}
-                href={href}
-                data-active={pathname === href}
-                className="flex items-center rounded-md px-3 py-2 transition-colors duration-500 hover:bg-slate-800/75 data-[active=true]:bg-slate-800"
-              >
-                <Icon className="mr-3 size-4" />
-                {title}
-              </Link>
-            ))}
-          </main>
+        <nav className="flex flex-col gap-2">
+          {navConfig.map(({ title, href, icon: Icon }) => (
+            <Link
+              key={title}
+              href={href}
+              data-active={pathname === href}
+              className="flex items-center rounded-md px-3 py-2 transition-colors duration-500 hover:bg-slate-800/75 data-[active=true]:bg-slate-800"
+            >
+              <Icon className="mr-3 size-4" />
+              {title}
+            </Link>
+          ))}
         </nav>
       </main>
+      <footer className="mt-auto border-t border-slate-800 p-6">
+        <span>{resolvedTheme}</span>
+        <Switch
+          checked={!!resolvedTheme && resolvedTheme === 'dark'}
+          onCheckedChange={(value) => {
+            if (value) {
+              setTheme('dark')
+              return
+            }
+
+            setTheme('light')
+          }}
+        />
+      </footer>
     </aside>
   )
 }
